f553e3f812512e04c2e752095c7a97ff66f010de
niglus <niglusn@gmail.com>
2019-12-02 05:42:41 +0700

    Menambahkan route dan file index

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/front', function () {
+    return view('front.index');
+});
 
-Auth::routes();
+/*Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');*/
