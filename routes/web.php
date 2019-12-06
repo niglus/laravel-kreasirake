8d99b12be8d4e35defb3ba04516d7119fc43367c
niglus <niglusn@gmail.com>
2019-12-06 13:16:26 +0700

    merubah file route dan menambahkan PagesController

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/front', function () {
-    return view('front.index');
-});
+});*/
+
+Route::get('/', 'PagesController@index');
 
 /*Auth::routes();
 
